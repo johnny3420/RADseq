7bbbb1364762c29ca97b7ea2144e31025bc496ce
johnny3420 <jtdavis@ucdavis.edu>
2016-07-29 14:54:14 -0700

    Cleaned up work and made names more clear

--- SiteMatching.R
+++ SiteMatching.R
@@ -5,9 +5,10 @@ library(Biostrings)
 library(GenomicRanges)
 
 ##Reading in Brassica FASTA file
+##Files from ftp://brassicadb.org/Brassica_napus/
 
 DNA <- readDNAStringSet("ChBrGSP/Brassica_napus_v4.1.chromosomes.fa.gz")
-testg <-readDNAStringSet("CHBrGSP/Brassica_napus.annotation_v5.gff3.cds.fa.gz")
+Gene <-readDNAStringSet("CHBrGSP/Brassica_napus.annotation_v5.gff3.cds.fa.gz")
 
 ###Preparing Pdict for Biostrings
 
@@ -17,10 +18,11 @@ name1 <- strsplit(enzyme, ",")
 name2 <- strsplit(sequence, ",")
 table <- cbind.data.frame(enzyme = name1, sequence =name2)
 colnames(table) <- c("enzyme", "sequence")
+table <- table[c(-3,-32,-39,-40),] #Removing enzymes with ambiguity codes
 pdict <- as.character(table[,2])
 names(pdict) <- table[,1]
 pdict <- DNAStringSet(pdict)
-pdict <- PDict(pdict, tb.start = 2, tb.width = 1)
+pdict <- PDict(pdict, tb.start = 1, tb.width = 6)
 
 ### 1st attempt at matching the pdict to the DNA Sequences
 
@@ -29,48 +31,48 @@ test <- vmatchPDict(pdict, DNA, fixed = "pattern") ### CANT BE RUN, FUNCTION IN 
                                               ###NEED RUN EACH TIME FOR EACH CHROMOSOME
 
 ### Counting occurance of each restriction site in each chromosome.
-counting <- lapply(1:41, function(s) {
+DNAcounting <- lapply(1:41, function(s) {
   countPDict(pdict, DNA[[s]])
 })
 
-names(counting)<- names(DNA)
+names(DNAcounting)<- names(DNA)
 
 ### Putting results in dataframe
 
-count.table <- data.frame(counting, row.names = table[,1])
-count.table$totalcuts <- rowSums(count.table)
-count.table$roughfraglen <- 1300000000/count.table$totalcuts
+DNAcount.table <- data.frame(DNAcounting, row.names = table[,1])
+DNAcount.table$totalcuts <- rowSums(DNAcount.table)
+DNAcount.table$roughfraglen <- 1300000000/DNAcount.table$totalcuts
 
 ### Identifies the locations of each restriction site for each enzyme on each chromsome.
-famatching <- lapply(1:41, function(s) {
+DNAmatching <- lapply(1:41, function(s) {
   matchPDict(pdict, DNA[[s]])
 })
-names(famatching) <- names(DNA) ## Puts names on each chromosome.
+names(DNAmatching) <- names(DNA) ## Puts names on each chromosome.
 
 
 ### Identifies the location of each restriction site for each gene in the annotated genome.
-annomatching <-lapply(1:length(testg), function(s) {
-  matchPDict(pdict,testg[[s]])
+Genematching <-lapply(1:length(Gene), function(s) {
+  matchPDict(pdict,Gene[[s]])
 })
-names(annomatching) <-names(testg)
+names(Genematching) <-names(Gene)
 ###when indexing with famatching[[x]][[y]] 
 ###x is chromosome and y is RE
 
 ###Isolates one gene and lists the enzymes that cut it and the locations of the cuts as a test
-test <- data.frame(annomatching$GSBRNA2T00121640001)
+test <- data.frame(Genematching$GSBRNA2T00121640001)
 test$group <- table[test$group,1]
 test[,1] <- table[test[,1],1]
 
 ###Creates a list of data frames of each gene and the enzymes
 ###which cut it and where
 ###
-listdf <- list()
-for(i in 1:length(annomatching)){
-  df <- data.frame(annomatching[i])
+genelist <- list()
+for(i in 1:length(Genematching)){
+  df <- data.frame(Genematching[i])
   listdf[[i]] <- df
 }
 
-names(listdf) <- names(testg)
+names(genelist) <- names(Gene)
 
 for(i in 1:length(listdf)){
   listdf[[i]][2] <- NULL
@@ -79,42 +81,9 @@ for(i in 1:length(listdf)){
 }
 
 ###
-### Scratch work due to problem with trusted bands
+###
 
-test.table <- table[c(-3,-32,-39,-40),]
-test.dict <- DNAStringSet(pdict)
-test.dict<-test.dict[c(-3, -32,-39,-40)]
-test.dict <- PDict(test.dict, tb.start = 1, tb.width = 6)
-test.dict
-
-test.counting <- lapply(1:41, function(s) {
-  countPDict(test.dict, DNA[[s]])
-})
-names(test.counting) <- names(DNA)
-test.count.table <- data.frame(test.counting, row.names = test.table[,1])
-test.count.table$totalcuts <- rowSums(test.count.table)
-test.count.table$roughfraglen <- 1300000000/test.count.table$totalcuts
-
-test.annomatching <-lapply(1:length(testg), function(s) {
-  matchPDict(test.dict,testg[[s]])
-})
-names(test.annomatching) <-names(testg)
-
-test.listdf <- list()
-for(i in 1:length(test.annomatching)){
-  df <- data.frame(test.annomatching[i])
-  test.listdf[[i]] <- df
-}
-
-names(test.listdf) <- names(testg)
-
-for(i in 1:length(test.listdf)){
-  test.listdf[[i]][2] <- NULL
-  colnames(test.listdf[[i]]) = c("enzyme", "start", "end", "width")
-  test.listdf[[i]][,1] <- table[test.listdf[[i]][,1],1]
-}
-
-cuts_per_gene <- sapply(test.listdf, function(x) table(x$enzyme))
+cuts_per_gene <- sapply(genelist, function(x) table(x$enzyme))
 
 cuts_all_genes <- rowSums(cuts_per_gene)
 
